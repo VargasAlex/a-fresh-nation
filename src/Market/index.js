864c86a4a1932a4c8cd913b887745274b0c6226b
Alex Vargas <alexv2489@gmail.com>
2018-08-07 14:40:16 -0400

    Displaying markets on submit

--- src/Market/index.js
+++ src/Market/index.js
@@ -5,32 +5,42 @@ class Market extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      zip: '',
       marketname: '',
       results: [],
     }
   }
 
-  componentDidMount() {
-    fetch('http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=10002')
-      .then(response => response.json())
+
+  onFormChange(evt) {
+    const domElement = evt.target.value;
+    this.setState({zip: domElement});
+  }
+
+  onSubmitClick(evt) {
+    fetch(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${this.state.zip}`)
+    .then(response => response.json())
       .then(marketname => {
-        console.log(marketname)
+        console.log(marketname.results)
         this.setState({
           results: marketname.results
-        })
-        console.log(this.state.results)
         });
-      }
+      });
+  }
 
   render () {
     return (
       <div className="Market">
         <h1>A Fresh Nation</h1>
-        {this.state.results.map(result => {
-          return (
-          <p>{result.marketname}</p>
-          )
-        })}
+        <form onChange={evt => this.onFormChange(evt)}>
+          <input name="zip" value={this.state.zip} />
+          <button type="button" onClick={evt => this.onSubmitClick(evt)}>Submit</button>
+          {this.state.results.map(result => {
+            return (
+              <p>{result.marketname.substring(3)}</p>
+            )
+          })}
+        </form>
       </div>
     )
   }
